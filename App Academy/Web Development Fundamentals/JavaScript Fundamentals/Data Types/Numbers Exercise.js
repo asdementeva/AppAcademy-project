32e59ec73793982dbb2baa44a6190bdaf841c16a
Alexandra Dementeva <102434370+asdementeva@users.noreply.github.com>
2023-07-30 11:44:50 +0300

    Add practice "Numbers Exsercise"

---
+++ App Academy/Web Development Fundamentals/JavaScript Fundamentals/Data Types/Numbers Exercise.js
@@ -0,0 +1,9 @@
+// Example:
+console.log(4 + 2.5)   // 6.5
+
+// Write 5 more expressions below:
+console.log(20 + 4);
+console.log(20 - 4);
+console.log(20 * 4);
+console.log(20 / 4);
+console.log(20 % 4);
