f2025483e1313ed1368701146abe866bc6d428b5
Alexandra Dementeva <102434370+asdementeva@users.noreply.github.com>
2023-08-05 00:46:54 +0300

    Add practice "Variable"

---
+++ App Academy/Web Development Fundamentals/JavaScript Fundamentals/Data Types/Variables Pracrice.js
@@ -0,0 +1,23 @@
+// 1. Declare a variable called `firstName` and assign it your first name as a string:
+let firstName = "Alex";
+
+// 2. Declare a variable called `lastName` and assign it your last name as a string:
+let lastName = "Dementeva";
+
+// 3. Declare a variable called `age`:
+let age;
+
+// 4. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+// 5. Assign the `age` variable to a number:
+age = 30;
+
+// 6. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+console.log(firstName);
+console.log(lastName);
+console.log(age);
